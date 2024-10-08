e827b7fca0477fc6c6baf444f9ed0be5d278d1a6
cunx <cunx@sapo.vn>
2024-10-08 14:42:59 +0700

    fix

--- source/wsgi.py
+++ source/wsgi.py
@@ -1,11 +1,17 @@
 from app import app, embedding_knowledge_thread, scheduler
 from kafka_consumer import kafka_thread
+import logging
+from flask import g
 
-@app.before_first_request
+@app.before_request
 def start_background_threads():
-    embedding_knowledge_thread.start()
-    kafka_thread.start()
-    scheduler.start()
+    try:
+        embedding_knowledge_thread.start()
+        kafka_thread.start()
+        scheduler.start()
+        g.background_threads_started = True
+    except Exception as e:
+        logging.info(f"Error starting threads: {str(e)}")
 
 if __name__ == "__main__":
     app.run(debug=True)
